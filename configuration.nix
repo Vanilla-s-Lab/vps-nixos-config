413815a77df4017ff288077cf1b79b039cbe9344
VergeDX <neko@hydev.org>
2021-08-19 00:19:07 +0000

    [-] configuration.nix: Comment original networking config.

--- configuration.nix
+++ configuration.nix
@@ -28,8 +28,8 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s5.useDHCP = true;
+  # networking.useDHCP = false;
+  # networking.interfaces.enp0s5.useDHCP = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
