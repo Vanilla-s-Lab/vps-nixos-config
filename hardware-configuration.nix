adf453a034fb4c012373c6dad7fee2f4c108066f
VergeDX <neko@hydev.org>
2021-08-19 00:04:47 +0000

    [+] Enable LISH.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -22,4 +22,10 @@
     [ { device = "/dev/sdb"; }
     ];
 
+  boot.kernelParams = [ "console=ttyS0,19200n8" ];
+  boot.loader.grub.extraConfig = ''
+    serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+    terminal_input serial;
+    terminal_output serial;
+  '';
 }
